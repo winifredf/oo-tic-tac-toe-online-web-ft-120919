56a5271d51a18c6b2664decc1a2b45e08811f946
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 11:17:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,3 +54,19 @@ end
       end
     end
   end
+
+class Game
+  attr_accessor :bo]ard, :player_1,  :player_2
+  
+  WIN_COMBINATIONS = [
+    [0,1,2,],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+  
