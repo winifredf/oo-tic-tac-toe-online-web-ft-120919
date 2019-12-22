734a88cfea6b2e048fa45ce6d47020962ca46955
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 12:18:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,3 +60,15 @@ end
   return  player
   end
   
+  def turn
+    puts  "Please choose a number 1-9:"
+    user_input = gets.chomp
+    index = input_to_index(user_input)
+    if  valid_move?(index)
+      player_token = current_player
+      move(index, player_token)
+      display_board
+    else
+      turn
+    end
+  end
