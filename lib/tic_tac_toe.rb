90b69c54b5f03c39bb39d7b5d34efd63f8e902f9
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 12:02:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,6 +19,6 @@ end
   def display_board
     puts  "#{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts  "-----------"
-    puts  "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts  "#{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts  "-----------"
-    puts  "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts  "#{@board[6]} | #{@board[7]} | #{@board[8]}"
