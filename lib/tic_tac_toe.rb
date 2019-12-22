22cb5ffb363af14a928d163b17294fce038af804
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-22 12:13:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,5 +37,17 @@ end
     !(@board[index].nil? || @board[index] == " ")
   end
   
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
   
+  def turn_count
+    turn = 0
+    @board.each do |index|
+      if  index == "X" || index == "0"
+        turn += 1
+      end
+    end
+    return  turn
+  end  
   
